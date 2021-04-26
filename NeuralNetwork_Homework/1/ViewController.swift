18a3bb3c4fae5eac807ad18f12eb4d27f5b240cc
Антон Бушманов <antonbusmanov@iMac-Anton.local>
2021-04-26 22:14:42 +0300

    first commit

---
+++ NeuralNetwork_Homework/1/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  NeuralNetwork_Homework
+//
+//  Created by Антон Бушманов on 13.02.2021.
+//
+
+import UIKit
+import CoreML
+import Vision
+import ImageIO
+
+class ViewController: UIViewController{
+   
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: ImageClassifier().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model \(error)" )
+        }
+    }()
+    
+    func updateClassifications(for image: UIImage) {
+        
+        classificationLabel.text = "Classifying..."
+        
+        
+        let orientation = CGImagePropertyOrientation(rawValue: UInt32(image.imageOrientation.rawValue))!
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Unable to create \(CIImage.self) from \(image)")
+        }
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                print("Failed to perform classification. \n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                self.classificationLabel.text = "Unable to classify image. \n\(error!.localizedDescription)"
+                return
+            }
+            let classifications = results as! [VNClassificationObservation]
+            if classifications.isEmpty {
+                self.classificationLabel.text = "Nothing recognized"
+            } else {
+                let topClassifications = classifications.prefix(2)
+                let descriptions = topClassifications.map { classification in
+                    return String(format: "   (%.2f) %@", classification.confidence, classification.identifier)
+                }
+                self.classificationLabel.text = "Classification: \n" + descriptions.joined(separator: "\n")
+            }
+        }
+    }
+    
+    
+    @IBAction func takePicture(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        let photoSourcePicker = UIAlertController()
+        let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .camera)
+        }
+        let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        
+        photoSourcePicker.addAction(takePhoto)
+        photoSourcePicker.addAction(choosePhoto)
+        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        updateClassifications(for: image)
+        
+    }
+}
---
+++ NeuralNetwork_Homework/2/VisionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  VisionViewController.swift
+//  NeuralNetwork_Homework
+//
+//  Created by Антон Бушманов on 16.02.2021.
+//
+
+import UIKit
+import CoreML
+import Vision
+import ImageIO
+
+class VisionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+
+
+}
